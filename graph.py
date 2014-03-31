6c6c675a0fe98c9d5beea491c8a941b32e9817eb
EdwardM9 <pator9@gmail.com>
2014-03-30 22:59:00 -0600

    Initial Commit.
    Code is currently based off the tutorial on charting found at
    newcoder.io

---
+++ graph.py
@@ -0,0 +1,71 @@
+"""
+Data Visualization
+
+Takes in parsed data from a CSV file and renders it onto a chart.
+
+Copyright(c) 2014 Moreno
+Distributed under the Creative Commons Attribution 3.0 Unported license.
+"""
+
+from collections import Counter
+
+import matplotlib.pyplot as plt
+import numpy.numarray as na
+
+import parse as p
+
+MY_FILE = "../data/sample_sfpd_incident_all.csv"
+
+def visualize_days():
+	#Parse data
+	data_file = p.parse(MY_FILE, ",")
+
+	#Make a counter from each day of the week
+	counter = Counter(item["DayOfWeek"] for item in data_file)
+	#seperate counter for ordering
+	data_list = [
+				counter["Sunday"],
+				counter["Monday"],
+				counter["Tuesday"],
+				counter["Wednesday"],
+				counter["Thursday"],
+				counter["Friday"],
+				counter["Saturday"]]
+	day_tuple = tuple(["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"])
+
+	# Assign the data to a plot
+	plt.plot(data_list)
+	plt.xticks(range(len(day_tuple)), day_tuple)
+	plt.savefig("Days.png")
+	plt.clf()
+	return
+
+def visualize_type():
+	#Parse data
+	data_file = p.parse(MY_FILE, ",")
+
+	#Make a counter from each category
+	counter = Counter(item["Category"] for item in data_file)
+	#Set labels
+	labels = tuple(counter.keys())
+	#Locations on x-axis
+	xlocations = na.array(range(len(labels))) + 0.5
+	
+	width = .5
+	
+	plt.bar(xlocations, counter.values(), width)
+	plt.xticks(xlocations + width / 2, labels, rotation = 90)
+	plt.subplots_adjust(bottom = .5)
+	plt.rcParams['figure.figsize'] = 12, 8
+	plt.savefig("Type.png")
+	plt.clf()
+
+	return
+
+def main():
+    visualize_days()
+    visualize_type()
+
+
+if __name__ == "__main__":
+    main()
---
+++ parse.py
@@ -0,0 +1,43 @@
+"""
+Data Visualization
+
+Copyright(c) 2014 E. Moreno
+Distributed under the Creative Commons Attribution 3.0 Unported license.
+"""
+
+import csv
+
+#Hardcoded path to file
+MY_FILE = "../data/sample_sfpd_incident_all.csv"
+
+def parse(raw_file, delimiter):
+	""" Parses a raw CSV file to a JSON-line object. """
+
+	# Open the CSV File
+	opened_file = open(raw_file)
+
+	# Read the CSV File
+	csv_data = csv.reader(opened_file, delimiter = delimiter)
+
+	parsed_data = []
+	#skip headers line
+	fields = csv_data.next()
+
+	for row in csv_data:
+		parsed_data.append(dict(zip(fields, row)))
+	# Close CSV File
+	opened_file.close()
+
+	# Build data structure
+	return parsed_data
+
+def main():
+    # Call our parse function and give it the needed parameters
+    new_data = parse(MY_FILE, ",")
+
+    # Let's see what the data looks like!
+    print new_data
+
+
+if __name__ == "__main__":
+    main()
